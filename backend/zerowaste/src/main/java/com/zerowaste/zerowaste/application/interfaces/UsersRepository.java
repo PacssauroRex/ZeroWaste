623501115bf1f735bb6ea14c203ec375deba1dc3
caiogmrocha <caioguilherme739@gmail.com>
2025-02-22 17:32:52 -0300

    feat(#6): define UsersRepository.findByEmail() annotation

--- backend/zerowaste/src/main/java/com/zerowaste/zerowaste/application/interfaces/UsersRepository.java
+++ backend/zerowaste/src/main/java/com/zerowaste/zerowaste/application/interfaces/UsersRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.zerowaste.zerowaste.domain.entities.user.User;
 
 public interface UsersRepository extends JpaRepository<User, Long> {
+    public User findByEmail(String email);
 }
