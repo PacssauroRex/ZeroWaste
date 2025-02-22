4ab423d244f29d8f1259095a1286198d2e4da3f1
caiogmrocha <caioguilherme739@gmail.com>
2025-02-22 12:55:25 -0300

    feat(#6): create /health endpoint
    To check health of the app

---
+++ backend/zerowaste/src/main/java/com/zerowaste/zerowaste/presentation/http/controllers/HealthCheckController.java
@@ -0,0 +1,21 @@
+package com.zerowaste.zerowaste.presentation.http.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class HealthCheckController {
+    @GetMapping("/health")
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        Map<String, String> responseBody = new HashMap<String, String>();
+
+        responseBody.put("message", "Hello World!");
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+}
