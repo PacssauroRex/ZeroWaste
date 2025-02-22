0bb1d68fb9ba334722ad183325e52fb8f3aa9792
caiogmrocha <caioguilherme739@gmail.com>
2025-02-22 17:52:57 -0300

    test(#6): ensure that RegisterUserService.execute() is able to register user

--- backend/zerowaste/src/test/java/com/zerowaste/zerowaste/application/services/users/RegisterUserServiceTest.java
+++ backend/zerowaste/src/test/java/com/zerowaste/zerowaste/application/services/users/RegisterUserServiceTest.java
@@ -1,15 +1,19 @@
 package com.zerowaste.zerowaste.application.services.users;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.zerowaste.zerowaste.application.interfaces.UsersRepository;
@@ -17,6 +21,7 @@ import com.zerowaste.zerowaste.application.services.users.exceptions.UserWithSam
 import com.zerowaste.zerowaste.domain.entities.user.User;
 import com.zerowaste.zerowaste.domain.entities.user.UserRole;
 
+@ExtendWith(MockitoExtension.class)
 public class RegisterUserServiceTest {
     @Autowired
     @InjectMocks
@@ -44,16 +49,31 @@ public class RegisterUserServiceTest {
         when(this.usersRepository.findByEmail(dto.email())).thenReturn(new User());
 
         // Act & Assert
-        assertThrows(UserWithSameEmailAlreadyExistsException.class, () -> {
-            registerUserService.execute(dto);
-        });
+        assertThrows(UserWithSameEmailAlreadyExistsException.class, () -> registerUserService.execute(dto));
     }
 
     @Test
-    @Disabled("Not implemented yet")
+    @DisplayName("It should be able to register user")
     public void itShouldRegisterUser() {
         // Arrange
-        // Act
-        // Assert
+        var dto = new RegisterUserServiceDto(
+            "John Doe",
+            "john@doe.com",
+            "some_strong_password",
+            UserRole.USER.getRole()
+        );
+
+        var user = new User();
+
+        user.setName(dto.name());
+        user.setEmail(dto.email());
+        user.setPassword(dto.password());
+        user.setRole(UserRole.valueOf(dto.role()));
+
+        when(this.usersRepository.findByEmail(dto.email())).thenReturn(null);
+
+        // Act & Assert
+        assertDoesNotThrow(() -> registerUserService.execute(dto));
+        verify(this.usersRepository, times(1)).save(user);
     }
 }
