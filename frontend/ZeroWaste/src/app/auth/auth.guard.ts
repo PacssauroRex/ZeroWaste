2163109e3535f655c71acd93f81af677420fd2cd
PacssauroRex <victorcauaorg@gmail.com>
2025-02-25 02:41:26 -0300

    Implementação do authGuard recebendo os dados da rota atual, obtenção da role exigida pela rota atual e comparação da role da rota com a role fornecida.

--- frontend/ZeroWaste/src/app/auth/auth.guard.ts
+++ frontend/ZeroWaste/src/app/auth/auth.guard.ts
@@ -1,5 +1,19 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from './auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const requiredRole: string | undefined = route.data?.['role'];
+
+  if(!authService.isAuthenticated()) {
+    return router.createUrlTree(['/login']);
+  }
+
+  if(requiredRole && !authService.hasRole(requiredRole)) {
+    return router.createUrlTree(['unauthorized'])
+  }
+  
   return true;
 };
