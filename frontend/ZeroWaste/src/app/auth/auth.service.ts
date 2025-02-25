5d41bf180b03e179670e36ce5e7b6167147e90c8
PacssauroRex <victorcauaorg@gmail.com>
2025-02-25 02:40:20 -0300

    Implementação do envio da tentativa de login para o back, criação do token, verificação de autenticação, obtenção e verificação da role requerida e obtenção do token no auth.service.

--- frontend/ZeroWaste/src/app/auth/auth.service.ts
+++ frontend/ZeroWaste/src/app/auth/auth.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { jwtDecode} from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiURL = "http://localhost:1717/auth";
+
+  async login(email: string, password: string): Promise<boolean> {
+    try{
+      const resposta = await fetch(this.apiURL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ email, password }),
+      });
+
+      if(!resposta.ok){
+        throw new Error('Login Falhou');
+      }
+
+      const data = await resposta.json();
+      localStorage.setItem('token', data.token);
+      return true;
+    } catch (error) {
+      console.error('Erro ao logar', error);
+      return false;
+    }
+  }
+}
+
+isAuthenticated(): boolean {
+  return !!this.getToken();
+}
+
+hasRole(requiredRole: string): boolean {
+  const token = this.getToken();
+  if (!token) {
+    return false;
+  }
+  try {
+    const decodedToken: any = jwtDecode(token);
+    return decodedToken.role === requiredRole;
+  } catch (error) {
+    console.error('Erro de token', error);
+    return false;    
+  }
+
+getToken(): string | null {
+  return localStorage.getIem('token');
+}
+
 }
