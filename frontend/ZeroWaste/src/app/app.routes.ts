5aaa3b3cb38c21217d45d4e68749f98f5f4bbb74
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-02-25 15:07:54 -0300

    Definição das rotas de login e registro

--- frontend/ZeroWaste/src/app/app.routes.ts
+++ frontend/ZeroWaste/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'login', pathMatch:'full'
+    },
+    {
+        path: 'login', component: LoginComponent, title: 'Login'
+    },
+    {
+        path: 'registro', component: RegisterComponent, title: 'Cadastro'
+    }
+];
