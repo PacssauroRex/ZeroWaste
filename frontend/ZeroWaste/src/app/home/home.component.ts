6fe4de317812bf5cd0172cab72fb392c3becc4c6
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-02-25 22:46:47 -0300

    Adição de um novo componente Home, que representa a homepage. E sua configuração no arquivo de rotas.

---
+++ frontend/ZeroWaste/src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  template: `
+    <h1>Home Page</h1>
+  `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
