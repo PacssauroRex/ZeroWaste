bf4d21673169967f0a3a2e38efdd9dd6643ca942
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-02 21:11:46 -0300

    feat(#42): create GetProductsController

---
+++ backend/zerowaste/src/main/java/com/zerowaste/controllers/products/GetProductsController.java
@@ -0,0 +1,34 @@
+package com.zerowaste.controllers.products;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zerowaste.services.products.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/products")
+public class GetProductsController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/")
+    public ResponseEntity<Map<String, ?>> getAllProducts() {
+        try {
+            return ResponseEntity.ok(Map.of("products", productService.getAll()));
+        }
+        catch (Exception err) {
+            return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("error", err.getMessage()));
+        }
+    }
+}
