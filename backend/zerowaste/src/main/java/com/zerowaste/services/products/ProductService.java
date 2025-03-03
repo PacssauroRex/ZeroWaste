863792edb5c2ec9439467fa5b96cb49ffbe1bec4
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-02 21:46:06 -0300

    feat(#39): add method getById

--- backend/zerowaste/src/main/java/com/zerowaste/services/products/ProductService.java
+++ backend/zerowaste/src/main/java/com/zerowaste/services/products/ProductService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.zerowaste.models.product.Product;
 import com.zerowaste.repositories.ProductsRepository;
+import com.zerowaste.services.products.exceptions.ProductNotFoundException;
 
 @Service
 public class ProductService {
@@ -19,4 +20,11 @@ public class ProductService {
         return productsRepository.findAll();
     }
     
+    public Product getById (Long id) throws ProductNotFoundException {
+        Optional<Product> p = productsRepository.findById(id);
+        if(!p.isPresent()) {
+            throw new ProductNotFoundException("Produto não encontrado");
+        }
+        return p.get();
+    }
 }
