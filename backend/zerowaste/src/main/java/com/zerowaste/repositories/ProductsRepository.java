78f73216f29f3f5330506dbce20c36b31858a0c7
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-03 00:00:19 -0300

    fix(#42): setting custom query to considerate soft deleting

--- backend/zerowaste/src/main/java/com/zerowaste/repositories/ProductsRepository.java
+++ backend/zerowaste/src/main/java/com/zerowaste/repositories/ProductsRepository.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.stereotype.Repository;
 
 import com.zerowaste.models.product.Product;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Product, Long> {
+
+    @NativeQuery("SELECT * FROM products WHERE deleted_at IS NULL")
+    List <Product> findAllNotDeleted();
+    
     Optional<Product> findById(Long id);
     List<Product> findAllByNameLike(String name);
 }
