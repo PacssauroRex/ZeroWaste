c36138c504a466d993224affc3bc5647c4e3807b
caiogmrocha <caioguilherme739@gmail.com>
2025-03-03 12:40:50 -0300

    git commit -m "feat(#33): create CreateProductDTO"

---
+++ backend/zerowaste/src/main/java/com/zerowaste/dtos/products/CreateProductDTO.java
@@ -0,0 +1,42 @@
+package com.zerowaste.dtos.products;
+
+import java.time.LocalDate;
+
+import com.zerowaste.models.product.ProductCategory;
+import com.zerowaste.utils.validation.ValidEnum.ValidEnum;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public record CreateProductDTO(
+    @NotEmpty(message = "O campo \"name\" é obrigatório")
+    @Size(max = 100, message = "O campo \"name\" deve ter entre 3 e 100 caracteres")
+    String name,
+
+    @NotEmpty(message = "O campo \"description\" é obrigatório")
+    @Size(max = 255, message = "O campo \"description\" deve ter entre 3 e 100 caracteres")
+    String description,
+
+    @NotEmpty(message = "O campo \"brand\" é obrigatório")
+    @Size(max = 100, message = "O campo \"brand\" deve ter entre 3 e 100 caracteres")
+    String brand,
+
+    @NotEmpty(message = "O campo \"category\" é obrigatório")
+    @ValidEnum(targetClassType = ProductCategory.class, message = "O campo \"category\" deve ser um dos valores: \"DAIRY\", \"FRUIT\", \"HYGIENE\", \"CLEANING\", \"DRINK\", \"MEAT\", \"BAKERY\"")
+    String category,
+
+    @NotEmpty(message = "O campo \"unitPrice\" é obrigatório")
+    @DecimalMin("0")
+    Double unitPrice,
+
+    @NotEmpty(message = "O campo \"stock\" é obrigatório")
+    @Min(0)
+    Integer stock,
+
+    @NotEmpty(message = "O campo \"expiresAt\" é obrigatório")
+    @Future(message = "O campo \"expiresAt\" deve ser uma data futura")
+    LocalDate expiresAt
+) {}
