6721a3f163703cf17e9522fe4ce17a3d963c1053
caiogmrocha <caioguilherme739@gmail.com>
2025-03-04 23:09:54 -0300

    feat(#33): create <app-input /> component

---
+++ frontend/ZeroWaste/src/app/components/form/input/input.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, forwardRef, signal } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  imports: [],
+  template: `
+    <input
+      [id]="id"
+      [name]="name"
+      [type]="type"
+      [placeholder]="placeholder"
+      (blur)="onTouch($event)"
+    />
+  `,
+  styleUrl: './input.component.css',
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ],
+  host: {
+    '(input)': 'onChange($event.target.value)',
+    '[value]': 'value',
+  },
+})
+export class InputComponent implements ControlValueAccessor {
+  // Input
+  @Input() id: HTMLInputElement['id'] = '';
+  @Input() name: HTMLInputElement['name'] = '';
+  @Input() type: HTMLInputElement['type'] = 'text';
+  @Input() placeholder: HTMLInputElement['placeholder'] = '';
+  @Input() value: HTMLInputElement['value'] = '';
+
+  // ControlValueAccessor
+  onChange: any = () => {};
+  onTouch: any = () => {};
+
+  constructor () {}
+
+  writeValue(value: any): void {
+    this.value = value;
+    this.onChange(this.value);
+    this.onTouch(this.value);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+}
