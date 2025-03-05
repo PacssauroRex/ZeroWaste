fb4dc6a1ec2f06a57abfad04c6267af153935bfd
caiogmrocha <caioguilherme739@gmail.com>
2025-03-04 23:41:09 -0300

    feat(#33): create <app-textarea /> component

---
+++ frontend/ZeroWaste/src/app/components/form/textarea/textarea.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-textarea',
+  imports: [],
+  template: `
+    <textarea
+      [id]="id"
+      [name]="name"
+      [placeholder]="placeholder"
+      [value]="value"
+      (blur)="onTouch($event)"
+    >{{ value }}</textarea>
+  `,
+  styleUrl: './textarea.component.css',
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextareaComponent),
+      multi: true,
+    },
+  ],
+  host: {
+    '(input)': 'onChange($event.target.value)',
+    '[value]': 'value'
+  },
+})
+export class TextareaComponent implements ControlValueAccessor {
+  @Input() id: HTMLInputElement['id'] = '';
+  @Input() name: HTMLInputElement['name'] = '';
+  @Input() type: HTMLInputElement['type'] = 'text';
+  @Input() placeholder: HTMLInputElement['placeholder'] = '';
+  @Input() value: HTMLInputElement['value'] = '';
+
+  onChange: any = () => {};
+  onTouch: any = () => {};
+
+  constructor () {}
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+    this.onChange(this.value);
+    this.onTouch(this.value);
+  }
+}
