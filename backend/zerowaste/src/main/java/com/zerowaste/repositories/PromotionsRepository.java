5febdd9cca800be6586fa204c697d5997ae9715f
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-07 02:06:41 -0300

    fix(#55): updating custom query

--- backend/zerowaste/src/main/java/com/zerowaste/repositories/PromotionsRepository.java
+++ backend/zerowaste/src/main/java/com/zerowaste/repositories/PromotionsRepository.java
@@ -7,12 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.stereotype.Repository;
 
-import com.zerowaste.dtos.promotions.PromotionDTO;
 import com.zerowaste.models.promotion.Promotion;
 
 @Repository
 public interface PromotionsRepository extends JpaRepository<Promotion, Long> {
-    @NativeQuery("SELECT name, percentage, starts_at, ends_at " +
-                "FROM promotions WHERE deleted_at IS NULL")
-    List<PromotionDTO> findAllNotDeleted();
+    @NativeQuery("SELECT * FROM promotions WHERE deleted_at IS NULL")
+    List<Promotion> findAllNotDeleted();
 }
