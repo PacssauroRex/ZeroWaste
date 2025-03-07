bbd759a885d83cfaa0c22f3c635df2c9c1996a8d
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-07 01:29:08 -0300

    fix(#55): changing findAll signature to findAllNotDeleted, adding a custom query, and changing its return to PromotionDTO

--- backend/zerowaste/src/main/java/com/zerowaste/repositories/PromotionsRepository.java
+++ backend/zerowaste/src/main/java/com/zerowaste/repositories/PromotionsRepository.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.stereotype.Repository;
 
+import com.zerowaste.dtos.promotions.PromotionDTO;
 import com.zerowaste.models.promotion.Promotion;
 
 @Repository
 public interface PromotionsRepository extends JpaRepository<Promotion, Long> {
-    List<Promotion> findAll();
+    @NativeQuery("SELECT name, percentage, starts_at, ends_at " +
+                "FROM promotions WHERE deleted_at IS NULL")
+    List<PromotionDTO> findAllNotDeleted();
 }
