4eb3ce1ce63d100dc22727128a809ec318be3e25
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-07 02:31:52 -0300

    feat(#53): adding getPromotionById method to PromotionService

--- backend/zerowaste/src/main/java/com/zerowaste/services/promotions/PromotionService.java
+++ backend/zerowaste/src/main/java/com/zerowaste/services/promotions/PromotionService.java
@@ -3,6 +3,7 @@ package com.zerowaste.services.promotions;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import com.zerowaste.dtos.promotions.AddPromotionDTO;
 import com.zerowaste.dtos.promotions.PromotionDTO;
 import com.zerowaste.models.promotion.Promotion;
 import com.zerowaste.repositories.PromotionsRepository;
+import com.zerowaste.services.promotions.exceptions.PromotionNotFoundException;
 
 @Service
 public class PromotionService {
@@ -36,5 +38,13 @@ public class PromotionService {
         }
 
         return promotionDTOs;
+    }
+
+    public Promotion getPromotionById(Long id) throws PromotionNotFoundException {
+        Optional<Promotion> p = promotionsRepository.findById(id);
+        if(!p.isPresent() || p.get().getDeletedAt() != null) 
+            throw new PromotionNotFoundException("Promoção não encontrada!");
+        
+        return p.get();
     }
 }
