f27e56951db36f3c8784aa21bd9442e97ec3db55
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-07 02:02:02 -0300

    feat(#55): adding getAllPromotions method to PŕomotionService

--- backend/zerowaste/src/main/java/com/zerowaste/services/promotions/PromotionService.java
+++ backend/zerowaste/src/main/java/com/zerowaste/services/promotions/PromotionService.java
@@ -1,11 +1,14 @@
 package com.zerowaste.services.promotions;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.zerowaste.dtos.promotions.AddPromotionDTO;
+import com.zerowaste.dtos.promotions.PromotionDTO;
 import com.zerowaste.models.promotion.Promotion;
 import com.zerowaste.repositories.PromotionsRepository;
 
@@ -23,5 +26,15 @@ public class PromotionService {
         p.setCreatedAt(LocalDate.now());
 
         promotionsRepository.save(p);
+    }
+
+    public List<PromotionDTO> getAllPromotions() {
+        List<Promotion> promos = promotionsRepository.findAllNotDeleted();
+        List<PromotionDTO> promotionDTOs = new ArrayList<>();
+        for (var p : promos) {
+            promotionDTOs.add(new PromotionDTO(p.getName(), p.getPercentage(), p.getStartsAt(), p.getEndsAt()));
+        }
+
+        return promotionDTOs;
     }
 }
