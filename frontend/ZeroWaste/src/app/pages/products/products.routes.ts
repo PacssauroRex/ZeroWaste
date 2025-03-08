37a2d71ed887c16b66419d821be80b78cc6d8ef9
caiogmrocha <caioguilherme739@gmail.com>
2025-03-08 02:04:00 -0300

    feat(#33): add AuthGuard to <create-product-form-page /> component

--- frontend/ZeroWaste/src/app/pages/products/products.routes.ts
+++ frontend/ZeroWaste/src/app/pages/products/products.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from "@angular/router";
 import { CreateProductFormPageComponent } from "./create-product-form-page/create-product-form-page.component";
+import { authGuard } from "../../auth/auth.guard";
 
 export const routes: Routes = [
   {
@@ -9,6 +10,7 @@ export const routes: Routes = [
         path: 'create',
         title: 'Create Product',
         component: CreateProductFormPageComponent,
+        canActivate: [authGuard],
       }
     ],
   },
