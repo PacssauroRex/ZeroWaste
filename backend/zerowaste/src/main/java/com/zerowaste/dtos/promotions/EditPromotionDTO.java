701460d767970d656af997372697b76a6caf5962
Guy176 <guy.paes176@gmail.com>
2025-03-07 21:53:53 -0300

    feat(#49): create EditPromotionDTO
    - implementa `EditPromotionDTO` para transferir dados para a edição de promoções.
    - valida os dados a serem transferidos

---
+++ backend/zerowaste/src/main/java/com/zerowaste/dtos/promotions/EditPromotionDTO.java
@@ -0,0 +1,28 @@
+package com.zerowaste.dtos.promotions;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public record EditPromotionDTO(
+        
+        @NotEmpty(message = "O campo \"name\" é obrigatório")
+        @Size(max = 100, message = "O campo \"name\" deve ter no máximo 100 caracteres")
+        String name,
+
+        @Min(0)
+        @Max(1)
+        Double percentage,
+
+        @FutureOrPresent
+        LocalDate startsAt,
+
+        @Future
+        LocalDate endsAt
+        
+        ) {}
