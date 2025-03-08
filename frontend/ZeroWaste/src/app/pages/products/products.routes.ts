78cc2b1bbef863d75b2af2064f8dc2eba9d92512
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-08 20:48:01 -0300

    feat(#43): setting routes to ListProductPageComponent e DetailProductPageComponent

--- frontend/ZeroWaste/src/app/pages/products/products.routes.ts
+++ frontend/ZeroWaste/src/app/pages/products/products.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from "@angular/router";
 import { CreateProductFormPageComponent } from "./create-product-form-page/create-product-form-page.component";
 import { authGuard } from "../../auth/auth.guard";
+import { ListProductPageComponent } from "./list-product-page/list-product-page.component";
+import { DetailProductPageComponent } from "./detail-product-page/detail-product-page.component";
 
 export const routes: Routes = [
   {
@@ -11,6 +13,18 @@ export const routes: Routes = [
         title: 'Create Product',
         component: CreateProductFormPageComponent,
         canActivate: [authGuard],
+      },
+      {
+        path: '',
+        title: 'Products',
+        component: ListProductPageComponent,
+        canActivate: [authGuard]
+      },
+      {
+        path: ':id',
+        title: 'Product detailing',
+        component: DetailProductPageComponent,
+        canActivate: [authGuard]
       }
     ],
   },
