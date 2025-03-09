93aeb8e85a5d06e7b910a43d194ec465cfe76955
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-09 18:32:21 -0300

    feat - add Products button into home page

--- frontend/ZeroWaste/src/app/home/home.component.ts
+++ frontend/ZeroWaste/src/app/home/home.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 import { LogoutComponent } from '../auth/logout/logout.component';
 import { ModalComponent } from "../components/modal/modal.component";
 import { ButtonComponent } from "../components/form/button/button.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [LogoutComponent],
+  imports: [
+    LogoutComponent, 
+    ButtonComponent, 
+    RouterModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
