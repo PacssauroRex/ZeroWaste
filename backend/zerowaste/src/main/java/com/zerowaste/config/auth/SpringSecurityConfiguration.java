84f4c6c28d2500b8f504f1706d1dff2bfaf455ff
PacssauroRex <victorcauaorg@gmail.com>
2025-03-09 18:57:25 -0300

    Mudança de {percentage} e {productsIds} para percentage e productsIds

--- backend/zerowaste/src/main/java/com/zerowaste/config/auth/SpringSecurityConfiguration.java
+++ backend/zerowaste/src/main/java/com/zerowaste/config/auth/SpringSecurityConfiguration.java
@@ -49,8 +49,8 @@ public class SpringSecurityConfiguration {
             //Promotions
             .requestMatchers(HttpMethod.POST, "/promotions/").hasRole("ADMIN")
             .requestMatchers(HttpMethod.GET, "/promotions/", "/promotions/**").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.GET, "/promotions/percentageFilter/{percentage}").hasAnyRole("USER", "ADMIN")
-            .requestMatchers(HttpMethod.GET, "/promotions/productFilter/{productsIds}").hasAnyRole("USER", "ADMIN")
+            .requestMatchers(HttpMethod.GET, "/promotions/percentageFilter/percentage").hasAnyRole("USER", "ADMIN")
+            .requestMatchers(HttpMethod.GET, "/promotions/productFilter/productsIds").hasAnyRole("USER", "ADMIN")
         )
         .addFilterBefore(authenticationUserService, UsernamePasswordAuthenticationFilter.class)
         .build();
