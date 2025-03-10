a6590240aaa9a4054212d634c1d25361408aec98
Guy176 <guy.paes176@gmail.com>
2025-03-10 19:51:56 -0300

    feat(#54): update promotions.routes
    - Atualização de rota para o detalhamento de uma promoção específica

--- frontend/ZeroWaste/src/app/pages/promotions/promotions.routes.ts
+++ frontend/ZeroWaste/src/app/pages/promotions/promotions.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from "@angular/router";
 import { authGuard } from "../../auth/auth.guard";
 import { CreatePromotionFormPageComponent } from "./create-promotion-form-page/create-promotion-form-page.component";
 import { UpdatePromotionFormPageComponent } from "./update-promotion-form-page/update-promotion-form-page.component";
+import { DetailProductPageComponent } from "../products/detail-product-page/detail-product-page.component";
+import { DetailPromotionPageComponent } from "./detail-promotion-page/detail-promotion-page.component";
 
 export const routes: Routes = [
   {
@@ -17,6 +19,12 @@ export const routes: Routes = [
         path: 'update/:id',
         title: 'Update Promotion',
         component: UpdatePromotionFormPageComponent,
+        canActivate: [authGuard],
+      },
+      {
+        path: ':id',
+        title: 'Promotion Detailing',
+        component: DetailPromotionPageComponent,
         canActivate: [authGuard],
       },
     ],
