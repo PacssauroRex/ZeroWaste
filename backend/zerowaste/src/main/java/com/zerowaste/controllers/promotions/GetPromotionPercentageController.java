f7683311a2fa215d3a2d488dd92db4a1c92158a7
PacssauroRex <victorcauaorg@gmail.com>
2025-03-12 13:26:37 -0300

    feat(#64)/feat(#65): changing RequestParam for PathVariable in GetPromotionPercentageController/GetPromotionProductIdController

--- backend/zerowaste/src/main/java/com/zerowaste/controllers/promotions/GetPromotionPercentageController.java
+++ backend/zerowaste/src/main/java/com/zerowaste/controllers/promotions/GetPromotionPercentageController.java
@@ -1,17 +1,15 @@
 package com.zerowaste.controllers.promotions;
 
+import com.zerowaste.services.promotions.GetPromotionPercentageService;
+import com.zerowaste.services.promotions.exceptions.PromotionNotFoundException;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.zerowaste.services.promotions.GetPromotionPercentageService;
-import com.zerowaste.services.promotions.exceptions.PromotionNotFoundException;
 
 @RestController
 @RequestMapping("/promotions")
@@ -22,7 +20,7 @@ public class GetPromotionPercentageController {
     private GetPromotionPercentageService getPromotionPercentageService;
 
     @GetMapping("/percentageFilter/{percentage}")
-    public ResponseEntity<Map<String, ?>> getPromotionByPercentage(@RequestParam Double percentage) {
+    public ResponseEntity<Map<String, ?>> getPromotionByPercentage(@PathVariable Double percentage) {
         try{
             return ResponseEntity.ok(Map.of("promotions", getPromotionPercentageService.execute(percentage)));
         } 
--- backend/zerowaste/src/main/java/com/zerowaste/controllers/promotions/GetPromotionProductIdController.java
+++ backend/zerowaste/src/main/java/com/zerowaste/controllers/promotions/GetPromotionProductIdController.java
@@ -1,17 +1,15 @@
 package com.zerowaste.controllers.promotions;
 
+import com.zerowaste.services.promotions.GetPromotionProductService;
+import com.zerowaste.services.promotions.exceptions.PromotionNotFoundException;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.zerowaste.services.promotions.GetPromotionProductService;
-import com.zerowaste.services.promotions.exceptions.PromotionNotFoundException;
 
 @RestController
 @RequestMapping("/promotions")
@@ -22,7 +20,7 @@ public class GetPromotionProductIdController {
     private GetPromotionProductService getPromotionProductService;
 
     @GetMapping("/productFilter/{productsIds}")
-    public ResponseEntity<Map<String, ?>> getPromotionByProductId(@RequestParam Long productsIds) {
+    public ResponseEntity<Map<String, ?>> getPromotionByProductId(@PathVariable Long productsIds) {
         try {
             return ResponseEntity.ok(Map.of("promotions", getPromotionProductService.execute(productsIds)));
         }
