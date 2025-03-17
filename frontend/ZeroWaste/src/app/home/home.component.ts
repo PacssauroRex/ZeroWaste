cc8ac2391e9862cd1b8c1181e9c07c65c1144111
Guy176 <guy.paes176@gmail.com>
2025-03-17 19:11:49 -0300

    refactor(#121): update home component
    - Remove o botão de logout da home

--- frontend/ZeroWaste/src/app/home/home.component.ts
+++ frontend/ZeroWaste/src/app/home/home.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { LogoutComponent } from '../auth/logout/logout.component';
-import { ModalComponent } from "../components/modal/modal.component";
 import { ButtonComponent } from "../components/form/button/button.component";
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [
-    LogoutComponent, 
-    ButtonComponent, 
+    ButtonComponent,
     RouterModule
   ],
   templateUrl: './home.component.html',
