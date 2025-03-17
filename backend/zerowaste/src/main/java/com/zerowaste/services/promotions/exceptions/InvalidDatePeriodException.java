21faf11ba59b86552b4cd5eb96b05e3942a33e9d
Guy176 <guy.paes176@gmail.com>
2025-03-17 18:08:29 -0300

    feature(#120): create InvalidDatePeriodException
    - Cria a classe de exceção para que seja utilizada no caso em que o período de datas é inválido.

---
+++ backend/zerowaste/src/main/java/com/zerowaste/services/promotions/exceptions/InvalidDatePeriodException.java
@@ -0,0 +1,9 @@
+package com.zerowaste.services.promotions.exceptions;
+
+public class InvalidDatePeriodException extends Exception {
+
+    public InvalidDatePeriodException(String message) {
+        super(message);
+    }
+
+}
