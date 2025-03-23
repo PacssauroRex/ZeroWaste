50a2b3438877a8623273cdc376e54d03d5ea5d4e
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-23 03:12:34 -0300

    feat(#143): adding deleteDonation method at DonationController

--- backend/zerowaste/src/main/java/com/zerowaste/controllers/donations/DonationController.java
+++ backend/zerowaste/src/main/java/com/zerowaste/controllers/donations/DonationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.zerowaste.dtos.donations.CreateDonationDTO;
 import com.zerowaste.dtos.donations.EditDonationDTO;
 import com.zerowaste.services.donations.CreateDonationService;
+import com.zerowaste.services.donations.DeleteDonationService;
 import com.zerowaste.services.donations.EditDonationService;
 import com.zerowaste.services.donations.GetDonationIdService;
 import com.zerowaste.services.donations.GetDonationService;
@@ -20,6 +21,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -34,13 +36,15 @@ public class DonationController {
     private final GetDonationService getDonationService;
     private final GetDonationIdService getDonationIdService;
     private final EditDonationService editDonationService;
+    private final DeleteDonationService deleteDonationService;
 
     public DonationController (CreateDonationService createDonationService, GetDonationService getDonationService, GetDonationIdService getDonationIdService,
-                               EditDonationService editDonationService) {
+                               EditDonationService editDonationService, DeleteDonationService deleteDonationService) {
         this.createDonationService = createDonationService;
         this.getDonationService = getDonationService;
         this.getDonationIdService = getDonationIdService;
         this.editDonationService = editDonationService;
+        this.deleteDonationService = deleteDonationService;
     }
 
     @PostMapping()
@@ -91,7 +95,7 @@ public class DonationController {
     }
     
     @PutMapping("/{id}")
-    public ResponseEntity<Map<String, ?>> editDonation(@PathVariable Long id, @RequestBody EditDonationDTO dto) {
+    public ResponseEntity<Map<String, ?>> editDonation(@PathVariable Long id, @RequestBody @Valid EditDonationDTO dto) {
         try {
             editDonationService.execute(id, dto);
             return ResponseEntity.ok(Map.of("message", "Doação editada com sucesso!"));
@@ -108,4 +112,21 @@ public class DonationController {
         }
     }
     
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, ?>> deleteDonation(@PathVariable Long id) {
+        try {
+            deleteDonationService.execute(id);
+            return ResponseEntity.ok(Map.of("message", "Doação deletada com sucesso!"));
+        } 
+        catch (DonationNotFoundException err) {
+            return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(Map.of("message", err.getMessage()));
+        }
+        catch(Exception err) {
+            return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("message", err.getMessage()));
+        }
+    }
 }
