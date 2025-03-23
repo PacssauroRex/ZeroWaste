d339109817d67eb712d13d035ebf83c0188d462e
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-23 00:44:29 -0300

    feat(#139): create DonationNotFoundException

---
+++ backend/zerowaste/src/main/java/com/zerowaste/services/donations/exceptions/DonationNotFoundException.java
@@ -0,0 +1,8 @@
+package com.zerowaste.services.donations.exceptions;
+
+public class DonationNotFoundException extends Exception{
+    
+    public DonationNotFoundException(String message) {
+        super(message);
+    }
+}
