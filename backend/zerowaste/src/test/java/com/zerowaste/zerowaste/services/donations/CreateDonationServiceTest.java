ffe9bf1f3a7d1d789641c4bb25b7b036866e1fe9
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-24 01:19:47 -0300

    feat - JUnit tests created for all Donation services

---
+++ backend/zerowaste/src/test/java/com/zerowaste/zerowaste/services/donations/CreateDonationServiceTest.java
@@ -0,0 +1,85 @@
+package com.zerowaste.zerowaste.services.donations;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.zerowaste.dtos.donations.CreateDonationDTO;
+import com.zerowaste.models.donation.Donation;
+import com.zerowaste.models.product.Product;
+import com.zerowaste.models.product.ProductCategory;
+import com.zerowaste.repositories.DonationsRepository;
+import com.zerowaste.repositories.ProductsRepository;
+import com.zerowaste.services.donations.CreateDonationService;
+import com.zerowaste.services.products.exceptions.ProductNotFoundException;
+
+@ExtendWith(MockitoExtension.class)
+public class CreateDonationServiceTest {
+    
+    private CreateDonationService createDonationService;
+
+    @Mock
+    private DonationsRepository donationsRepository;
+
+    @Mock
+    private ProductsRepository productsRepository;
+
+    @BeforeEach
+    public void setUp () {
+        MockitoAnnotations.openMocks(this);
+        this.createDonationService = new CreateDonationService(donationsRepository, productsRepository);
+    }
+
+    @Test
+    public void createDonationTest() throws ProductNotFoundException { //Criação de doação bem sucedida
+        Long productId = 1L;
+        Product product = new Product(
+            productId,
+            "Teste",
+            "Teste", 
+            "Teste", 
+            ProductCategory.MEAT, 
+            10.0, 
+            8.0,
+            20,
+            LocalDate.now().plusDays(1),
+            LocalDate.now(),
+            null,
+            null
+        );
+        CreateDonationDTO dto = new CreateDonationDTO("Doação teste", Arrays.asList(productId), LocalDate.now());
+
+        //Mocking do comportamento dos repositórios
+        when(productsRepository.findById(productId)).thenReturn(Optional.of(product));  
+
+        //Salvando a doação
+        createDonationService.execute(dto);
+
+        //Verificando se o repositório foi chamado para salvar a doação
+        verify(donationsRepository, times(1)).save(any(Donation.class));
+    }
+
+    @Test
+    public void createDonationFailTest() { //Tentativa de criação de doação com produto inválido
+        Long productId = 1L;
+        CreateDonationDTO dto = new CreateDonationDTO("Doação teste", Arrays.asList(productId), LocalDate.now());
+
+        //Checando se exceção correta é lançada
+        assertThrows(ProductNotFoundException.class, () -> {
+            createDonationService.execute(dto);
+        });
+    }
+}
---
+++ backend/zerowaste/src/test/java/com/zerowaste/zerowaste/services/donations/GetDonationIdServiceTest.java
@@ -0,0 +1,94 @@
+package com.zerowaste.zerowaste.services.donations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.zerowaste.models.donation.Donation;
+import com.zerowaste.repositories.DonationsRepository;
+import com.zerowaste.services.donations.GetDonationIdService;
+import com.zerowaste.services.donations.exceptions.DonationNotFoundException;
+
+public class GetDonationIdServiceTest {
+    @Mock
+    private DonationsRepository donationsRepository;
+
+    private GetDonationIdService getDonationIdService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        getDonationIdService = new GetDonationIdService(donationsRepository);
+    }
+
+    @Test
+    public void getDonationByIdTest() throws DonationNotFoundException { //Busca realizada com sucesso
+        //Criando doação
+        Long donationId = 1L;
+        Donation donation = new Donation();
+        donation.setId(donationId);
+        donation.setName("Doação teste");
+        donation.setDate(LocalDate.now());
+        donation.setDeletedAt(null); //Garantir que a doação não está deletada
+
+        //Mocking do comportamento do repositório
+        when(donationsRepository.findById(donationId)).thenReturn(Optional.of(donation));
+
+        //Executando método
+        Donation result = getDonationIdService.execute(donationId);
+
+        //Verificando se a doação foi retornada corretamente
+        assertNotNull(result);
+        assertEquals(donationId, result.getId());
+
+        //Verificando que o repositório foi chamado uma vez
+        verify(donationsRepository, times(1)).findById(donationId);
+    }
+
+    @Test
+    public void getDonationByIdFailTest1() { //Buscando doação com id inválido (não existente)
+        Long donationId = 1L;
+        when(donationsRepository.findById(donationId)).thenReturn(Optional.empty());
+
+        //Verificando se a exceção é lançada
+        assertThrows(DonationNotFoundException.class, () -> {
+            getDonationIdService.execute(donationId);
+        });
+
+        //Verificando se o repositório foi chamado uma vez
+        verify(donationsRepository, times(1)).findById(donationId);
+    }
+
+    @Test
+    public void getDonationByIdFailTest2() { //Buscando doação deletada
+        //Criando doação
+        Long donationId = 1L;
+        Donation donation = new Donation();
+        donation.setId(donationId);
+        donation.setName("Donation 1");
+        donation.setDate(LocalDate.now());
+        donation.setDeletedAt(LocalDate.now()); //Settando data de deleção
+
+        //Mocking do comportamento do repositório
+        when(donationsRepository.findById(donationId)).thenReturn(Optional.of(donation));
+
+        //Verificando se a exceção é lançada quando a doação está deletada
+        assertThrows(DonationNotFoundException.class, () -> {
+            getDonationIdService.execute(donationId);
+        });
+
+        //Verificando se o repositório foi chamado uma vez
+        verify(donationsRepository, times(1)).findById(donationId);
+    }
+}
