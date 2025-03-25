682f5f4a6e3bed0021a65a2a830e3726af91f746
Guy176 <guy.paes176@gmail.com>
2025-03-25 04:23:21 -0300

    feat(donation-points): create Exceptions

---
+++ backend/zerowaste/src/main/java/com/zerowaste/services/donation_points/exceptions/DonationPointNotFoundException.java
@@ -0,0 +1,9 @@
+package com.zerowaste.services.donation_points.exceptions;
+
+public class DonationPointNotFoundException extends Exception {
+
+    public DonationPointNotFoundException(String message) {
+        super(message);
+    }
+    
+}
