4640d21095de2207c2c4b48ec8d1f909d6c29747
PacssauroRex <victorcauaorg@gmail.com>
2025-03-25 23:14:51 -0300

    Creating the if, for, etc.. tests for EditPromotionServiceTest.java

--- backend/zerowaste/src/test/java/com/zerowaste/zerowaste/services/promotions/EditPromotionServiceTest.java
+++ backend/zerowaste/src/test/java/com/zerowaste/zerowaste/services/promotions/EditPromotionServiceTest.java
@@ -1,21 +1,35 @@
 package com.zerowaste.zerowaste.services.promotions;
 
+import com.zerowaste.dtos.products.CreateProductDTO;
+import com.zerowaste.dtos.products.EditProductDTO;
 import com.zerowaste.dtos.promotions.AddPromotionDTO;
 import com.zerowaste.dtos.promotions.EditPromotionDTO;
+import com.zerowaste.models.product.Product;
+import com.zerowaste.models.product.ProductCategory;
 import com.zerowaste.models.promotion.Promotion;
 import com.zerowaste.repositories.ProductsRepository;
 import com.zerowaste.repositories.PromotionsRepository;
+import com.zerowaste.services.products.exceptions.ProductNotFoundException;
 import com.zerowaste.services.promotions.EditPromotionService;
+import com.zerowaste.services.promotions.exceptions.InvalidDatePeriodException;
+import com.zerowaste.services.promotions.exceptions.PromotionNotFoundException;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -32,7 +46,7 @@ public class EditPromotionServiceTest {
     private PromotionsRepository promotionsRepository;
 
     @Mock
-    private ProductsRepository productsRepository;
+    private ProductsRepository productRepository;
 
     @BeforeEach
     public void setup() {
@@ -43,13 +57,12 @@ public class EditPromotionServiceTest {
     @DisplayName("It should be able to edit a promotion")
     public void itShouldEditPromotion() {
 
-         // Arrange
+        // Arrange
         var dto = new AddPromotionDTO(
-            "Promotion Name",
-            15,
-            LocalDate.now().minusDays(1),
-            LocalDate.now().plusDays(1)
-        );
+                "Promotion Name",
+                15,
+                LocalDate.now().minusDays(1),
+                LocalDate.now().plusDays(1));
 
         var promotion = new Promotion();
 
@@ -62,22 +75,147 @@ public class EditPromotionServiceTest {
         promotion.setCreatedAt(LocalDate.now());
 
         var dtoEdit = new EditPromotionDTO(
-            "Promotion Ed Name",
-            20,
-            java.time.LocalDate.now().minusDays(2),
-            java.time.LocalDate.now().plusDays(2), 
-            Set.of()
-        );
+                "Promotion Ed Name",
+                20,
+                java.time.LocalDate.now().minusDays(2),
+                java.time.LocalDate.now().plusDays(2),
+                Set.of());
 
         promotion.setName(dtoEdit.name());
         promotion.setPercentage(dtoEdit.percentage());
         promotion.setStartsAt(dtoEdit.startsAt());
         promotion.setEndsAt(dtoEdit.endsAt());
-    
+
         // Act & Assert
         when(promotionsRepository.findById(id)).thenReturn(Optional.of(promotion));
         assertDoesNotThrow(() -> sut.execute(id, dtoEdit));
         verify(this.promotionsRepository, times(1)).save(promotion);
         verify(this.promotionsRepository, times(1)).findById(id);
     }
+
+    @Test
+    @DisplayName("It should throw PromotionNotFoundException")
+    public void itShouldThrowExceptionForPromotionNotFound() {
+        // Arrange
+
+        Long id = 9999L;
+
+        var dto = new EditPromotionDTO(
+                "Promotion Ed Name",
+                20,
+                java.time.LocalDate.now().minusDays(2),
+                java.time.LocalDate.now().plusDays(2),
+                Set.of());
+
+        when(promotionsRepository.findById(id)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(NoSuchElementException.class, () -> sut.execute(id, dto));
+    }
+
+    @Test
+    @DisplayName("It should throw InvalidDatePeriodException")
+    public void itShouldThrowExceptionForInvalidDateRange() {
+        // Arrange
+
+        Long id = 1l;
+        var dto = new AddPromotionDTO(
+                "Promotion Name",
+                15,
+                LocalDate.now().plusDays(2),
+                LocalDate.now().plusDays(2));
+
+        var promotion = new Promotion();
+
+        promotion.setName(dto.name());
+        promotion.setId(id);
+        promotion.setPercentage(dto.percentage());
+        promotion.setStartsAt(dto.startsAt());
+        promotion.setEndsAt(dto.endsAt());
+        promotion.setCreatedAt(LocalDate.now());
+
+        var dtoEdit = new EditPromotionDTO(
+                "Promotion Ed Name",
+                20,
+                java.time.LocalDate.now().plusDays(5),
+                java.time.LocalDate.now().plusDays(2),
+                Set.of());
+
+        promotion.setName(dtoEdit.name());
+        promotion.setPercentage(dtoEdit.percentage());
+        promotion.setStartsAt(dtoEdit.startsAt());
+        promotion.setEndsAt(dtoEdit.endsAt());
+
+        when(promotionsRepository.findById(id)).thenReturn(Optional.of(promotion));
+        // Act & Assert
+        assertThrows(InvalidDatePeriodException.class, () -> sut.execute(id, dtoEdit));
+    }
+
+    @Test
+    @DisplayName("It should throw ProductNotFoundException")
+    public void itShouldThrowExceptionWhenNoValidProductsAreFound() {
+        // Arrange
+
+        Long id = 9999L;
+
+        var dto = new EditPromotionDTO(
+                "Promotion Ed Name",
+                20,
+                java.time.LocalDate.now().minusDays(2),
+                java.time.LocalDate.now().plusDays(2),
+                Set.of(1L, 2L));
+
+        Promotion promotion = new Promotion();
+        promotion.setId(id);
+        promotion.setName(dto.name());
+        promotion.setPercentage(dto.percentage());
+        promotion.setStartsAt(dto.startsAt());
+        promotion.setEndsAt(dto.endsAt());
+
+        when(promotionsRepository.findById(id)).thenReturn(Optional.of(promotion));
+
+        // Act & Assert
+        assertThrows(ProductNotFoundException.class, () -> sut.execute(id, dto));
+    }
+
+    @Test
+    @DisplayName("Deve atualizar corretamente o preço promocional dos produtos")
+    void itShouldUpdatePromotionalPriceOfProducts() throws PromotionNotFoundException, ProductNotFoundException, InvalidDatePeriodException {
+        // Arrange
+        Long id = 1l;
+        Long productId = 1l;
+
+        Product product = new Product();
+        product.setId(productId);
+        product.setName("Product N");
+        product.setDescription("aaaa");
+        product.setBrand("aaaaa");
+        product.setCategory(ProductCategory.HYGIENE);
+        product.setUnitPrice(20.0);
+        product.setStock(20);
+        product.setCreatedAt(LocalDate.now());
+        product.setExpiresAt(LocalDate.now().plusDays(1));
+
+        var dto = new EditPromotionDTO(
+                "Promotion Ed Name",
+                20,
+                java.time.LocalDate.now().minusDays(2),
+                java.time.LocalDate.now().plusDays(2),
+                Set.of(productId)
+        );
+
+        Promotion promotion = new Promotion();
+        promotion.setId(id);
+        promotion.setName(dto.name());
+        promotion.setPercentage(dto.percentage());
+        promotion.setStartsAt(dto.startsAt());
+        promotion.setEndsAt(dto.endsAt());
+
+        when(promotionsRepository.findById(id)).thenReturn(Optional.of(promotion));
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+
+        assertDoesNotThrow(() -> sut.execute(id, dto));
+
+    }
+
 }
