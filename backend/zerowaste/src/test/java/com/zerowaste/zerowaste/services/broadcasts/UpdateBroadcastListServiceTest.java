82b443877a210971823bf29d13b66d501372795e
caiogmrocha <caioguilherme739@gmail.com>
2025-03-30 20:30:26 -0300

    test(#178): ensure that UpdateBroadcastListService store new e-mails when they are not found

--- backend/zerowaste/src/test/java/com/zerowaste/zerowaste/services/broadcasts/UpdateBroadcastListServiceTest.java
+++ backend/zerowaste/src/test/java/com/zerowaste/zerowaste/services/broadcasts/UpdateBroadcastListServiceTest.java
@@ -2,13 +2,14 @@ package com.zerowaste.zerowaste.services.broadcasts;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,6 +19,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.zerowaste.dtos.broadcasts.UpdateBroadcastListDTO;
+import com.zerowaste.models.broadcast.BroadcastEmail;
 import com.zerowaste.models.broadcast.BroadcastList;
 import com.zerowaste.models.broadcast.BroadcastListSendProtocol;
 import com.zerowaste.models.broadcast.BroadcastListSendType;
@@ -141,11 +143,47 @@ public class UpdateBroadcastListServiceTest {
     }
 
     @Test
-    @Disabled("It should store new e-mails when they are not found")
+    @DisplayName("It should store new e-mails when they are not found")
     public void itShouldStoreNewEmails() {
         // Arrange
-        // var product = new Product();
+        var product = new Product();
 
-        // product.setId(1L);
+        product.setId(1L);
+        product.setName("Product 1");
+        product.setDescription("Product 1 description");
+        product.setBrand("Brand 1");
+        product.setCategory(ProductCategory.HYGIENE);
+        product.setUnitPrice(10.0);
+
+        when(productsRepository.findAllById(List.of(product.getId()))).thenReturn(List.of(product));
+
+        var broadcastList = new BroadcastList();
+
+        broadcastList.setId(1L);
+        broadcastList.setName("Promoção de Páscoa");
+        broadcastList.setSendProtocol(BroadcastListSendProtocol.EMAIL);
+        broadcastList.setSendType(BroadcastListSendType.MANUAL);
+        broadcastList.setProducts(List.of(product));
+
+        when(broadcastListRepository.findById(broadcastList.getId())).thenReturn(Optional.of(broadcastList));
+
+        var broadcastEmail = new BroadcastEmail();
+
+        broadcastEmail.setEmail("john@doe.com");
+
+        List<Long> productsIds = List.of(product.getId());
+        List<String> emails = List.of(broadcastEmail.getEmail());
+
+        var dto = new UpdateBroadcastListDTO(
+            "Promoção de Páscoa",
+            "BOMBANDO! Produtos de Páscoa com até 50% de desconto",
+            BroadcastListSendType.MANUAL.toString(),
+            emails,
+            productsIds
+        );
+
+        // Act & Assert
+        assertDoesNotThrow(() -> sut.execute(product.getId(), dto));
+        verify(broadcastEmailsRepository, times(1)).saveAll(List.of(broadcastEmail));
     }
 }
