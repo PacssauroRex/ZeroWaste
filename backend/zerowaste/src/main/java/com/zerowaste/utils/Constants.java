f5c5df537efa966bdb3199dab0f7903bd67be194
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-30 05:12:52 -0300

    fix - adding private constructor to Constants file

--- backend/zerowaste/src/main/java/com/zerowaste/utils/Constants.java
+++ backend/zerowaste/src/main/java/com/zerowaste/utils/Constants.java
@@ -3,4 +3,6 @@ package com.zerowaste.utils;
 public class Constants {
     public static final String message = "message";
     public static final String generalExceptionCatchText = "Houve algum problema interno: ";
+
+    private Constants() {}
 }
