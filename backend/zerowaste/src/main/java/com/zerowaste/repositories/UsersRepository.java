d3bb2e037ca3364caf3ad464983158ec4d7a81f6
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-31 02:03:22 -0300

    fix - adding Repository annotation at UsersRepository

--- backend/zerowaste/src/main/java/com/zerowaste/repositories/UsersRepository.java
+++ backend/zerowaste/src/main/java/com/zerowaste/repositories/UsersRepository.java
@@ -1,9 +1,11 @@
 package com.zerowaste.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.zerowaste.models.user.User;
 
+@Repository
 public interface UsersRepository extends JpaRepository<User, Long> {
     public User findByEmail(String email);
 }
