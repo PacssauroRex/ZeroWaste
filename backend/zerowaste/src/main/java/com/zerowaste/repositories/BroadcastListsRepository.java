af3bc2797b2089971f64aada495cdfb5bfdb8397
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-03-31 14:49:42 -0300

    feat(#181): create findAllNotDeleted method

--- backend/zerowaste/src/main/java/com/zerowaste/repositories/BroadcastListsRepository.java
+++ backend/zerowaste/src/main/java/com/zerowaste/repositories/BroadcastListsRepository.java
@@ -1,9 +1,16 @@
 package com.zerowaste.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.stereotype.Repository;
 
 import com.zerowaste.models.broadcast.BroadcastList;
+import com.zerowaste.models.donation_point.DonationPoint;
 
 @Repository
-public interface BroadcastListsRepository extends JpaRepository<BroadcastList, Long> {}
+public interface BroadcastListsRepository extends JpaRepository<BroadcastList, Long> {
+    @NativeQuery("SELECT * FROM broadcast_emails WHERE deleted_at IS NULL")
+    List<DonationPoint> findAllNotDeleted();
+}
