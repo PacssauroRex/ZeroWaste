12d196801f123b6a6de0c04d07fb9e099f39cf26
caiogmrocha <caioguilherme739@gmail.com>
2025-04-05 22:44:12 -0300

    feat(#182): create app-update-broadcast-list-form-page

--- frontend/ZeroWaste/src/app/pages/broadcasts/broadcast.routes.ts
+++ frontend/ZeroWaste/src/app/pages/broadcasts/broadcast.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { authGuard } from "../../auth/auth.guard";
 import { ListBroadcastPageComponent } from "./list-broadcast-page/list-broadcast-page.component";
 import { CreateBroadcastListFormPageComponent } from "./create-broadcast-list-form-page/create-broadcast-list-form-page.component";
+import { UpdateBroadcastListFormPageComponent } from "./update-broadcast-list-form-page/update-broadcast-list-form-page.component";
 
 export const routes: Routes = [
   {
@@ -17,6 +18,13 @@ export const routes: Routes = [
         path: 'create',
         title: 'Cadastrar lista de transmissão',
         component: CreateBroadcastListFormPageComponent,
+        canActivate: [authGuard],
+        data: { role: 'ADMIN' },
+      },
+      {
+        path: 'update/:id',
+        title: 'Atualizar lista de transmissão',
+        component: UpdateBroadcastListFormPageComponent,
         canActivate: [authGuard],
         data: { role: 'ADMIN' },
       },
